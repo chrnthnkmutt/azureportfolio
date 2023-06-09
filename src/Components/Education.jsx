e3e77f88d54962ac094f95839d2a40a44aafbd25
Charunthon Limseelo <charunthon.lims@kmutt.ac.th>
2023-06-09 22:08:34 +0700

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 const Education = () => {
     return(
-        <section className="light" id="education">
-            <h2>Education</h2>
+        <section className="padding" id="portfolio">
+            <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+            <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+            <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+            <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out os 1 slideIn" }}
+            alt={imageAltText}
+          />
+        </div>
+        </div>
         </section>
-    )
-}
+    );
+};
 
 export default Education;
