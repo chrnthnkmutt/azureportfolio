23b795c51d1f23329076c9a7b7d0ab2bbadb6ff9
Charunthon Limseelo <charunthon.lims@kmutt.ac.th>
2023-06-09 17:29:51 +0700

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  */
 import image from "../images/design-desk.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "The picture that I have some discussion with my professor";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "Azure Static Web Apps 🎉",
+    title: "My Another Personal Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Creating a personal website with simple HTML, CSS, and JavaScript. This is a great way to learn the basics of web development.",
+    url: "https://boatchrnthn.netlify.app",
   },
   {
-    title: "Web Development",
+    title: "[Microsoft Dev Skill Ep.1] How to create your first Visual Studio Code Extension",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Make the visual studio code extension on VS-code, by using some library and tricks from Node.js and TypeScript",
+    url: "https://medium.com/@boatchrnthn/microsoft-dev-skill-ep-1-how-to-create-your-first-visual-studio-code-extension-f1384419076a",
   },
   {
-    title: "My Resume Site",
+    title: "[Microsoft Dev Skill Ep.2] Say hello to Azure OpenAI Service and Semantic Kernel",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Overview of what I learned and what I created during the workshop. I hope you enjoy reading it and get inspired to try out OpenAI yourself!",
+    url: "https://medium.com/@boatchrnthn/microsoft-dev-skill-ep-2-say-hello-to-openai-service-and-semantic-kernel-6a6cf8d5b2e1",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "[Microsoft Dev Skill Ep.3] Level Up with GitHub Codespaces and Copilot",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I will introduce you to these features and show you how they can help you level up your coding skills using GitHub Codespaces and GitHub Copilot.",
+    url: "https://medium.com/@boatchrnthn/microsoft-dev-skill-ep-3-level-up-with-github-codespaces-and-copilot-5b85a5fd1b2a",
   },
 ];
 
