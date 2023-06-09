9c10c1cb20fe7be877e4b4bbb0ff4402c21d5145
Charunthon Limseelo <charunthon.lims@kmutt.ac.th>
2023-06-09 22:15:11 +0700

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,7 +4,36 @@ const Education = () => {
     return(
         <section className="light" id="education">
             <h2 style={{ textAlign: "center" }}>Education</h2>
-            
+            <ol class="timeline-list">
+            <li class="timeline-item">
+
+              <h4 class="h4 timeline-item-title">King Mongkut's University of Technology Thonburi</h4>
+
+              <span>2022 - Present</span>
+
+              <p class="timeline-text">
+              At this moment, I am studying in the second year of my bachelor's degree in Computer Engineering (International Program) or CPE at KMUTT.
+              </p>
+              &nbsp;
+              <p class="timeline-text">
+              During these year, I would have some collaborations with profeesors and friends in order to do some projects.
+              </p>
+
+
+            <li class="timeline-item">
+
+              <h4 class="h4 timeline-item-title">Assumption College Samutprakarn</h4>
+
+              <span>2010 - 2022</span>
+
+              <p class="timeline-text">
+                I've studied in this school since I was in Primary 1. I've learned many things from this school and I've met many friends. Also,
+                This place is a start of my journey to learn English, Computer Science, and creating many innovations for the school education.
+              </p>
+
+            </li>
+
+          </ol>
         </section>
     )
 }
