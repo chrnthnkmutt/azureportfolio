0d713d5f1001a2f9621d75ac8d1c6ed474b538c3
Charunthon Limseelo <charunthon.lims@kmutt.ac.th>
2023-06-09 22:10:33 +0700

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
 const Education = () => {
-    return(
-        <section className="padding" id="portfolio">
-            <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-            <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-            <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+    return (
+      <section className="padding" id="education">
+        <h2 style={{ textAlign: "center" }}>Education</h2>
+        <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+          <div style={{ maxWidth: "40%", alignSelf: "center" }}>
             <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out os 1 slideIn" }}
-            alt={imageAltText}
-          />
+              src={image}
+              style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out os 1 slideIn" }}
+              alt={imageAltText}
+            />
+          </div>
+          <div className="container">
+            {projectList.map((project) => (
+              <div className="box" key={project.title}>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+                </a>
+                <p className="small">{project.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        </div>
-        </section>
+      </section>
     );
-};
+  };
 
 export default Education;
